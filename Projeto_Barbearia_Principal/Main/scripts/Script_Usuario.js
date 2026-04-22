78c6074d5aead7cff896e33815b5ebcbc5e5ef42
danngui <danielguilherminho@outlook.com>
2026-04-21 22:52:32 -0300

    Mudanças Geral

--- Projeto_Barbearia_Principal/Main/scripts/Script_Usuario.js
+++ Projeto_Barbearia_Principal/Main/scripts/Script_Usuario.js
@@ -1,21 +1,23 @@
-// Aguarda o HTML carregar totalmente, não remover nada do java script.
 document.addEventListener('DOMContentLoaded', () => {
     
-
     const botoes = document.querySelectorAll('.menu-item');
     const secoes = document.querySelectorAll('.content-section');
 
+    // 🔥 limpa qualquer active bugado
+    secoes.forEach(s => s.classList.remove('active'));
+    botoes.forEach(b => b.classList.remove('active'));
+
+    // define inicial
+    document.getElementById("Perfil").classList.add("active");
+    document.querySelector('[data-target="Perfil"]').classList.add("active");
 
     botoes.forEach(botao => {
         botao.addEventListener('click', () => {
             
-
             const alvoId = botao.getAttribute('data-target');
-
 
             botoes.forEach(b => b.classList.remove('active'));
             secoes.forEach(s => s.classList.remove('active'));
-
 
             botao.classList.add('active');
 
